ea8f1147e0618814d66fa5e48ac823bb2abb6dee
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2009-10-03 23:32:51 -0400

    basic implementation of namaste w/ unit tests

--- namaste.py
+++ namaste.py
@@ -1,35 +1,84 @@
+from __future__ import with_statement
+import os
+import glob
+
 def main():
     import optparse    
-    parser = optparse.OptionParser()
+    parser = optparse.OptionParser("Usage: %prog [options] <cmd> [<value>]")
+    parser.add_option("-d", dest="directory", default=os.getcwd(), 
+                      help="directory")
     options, args = parser.parse_args()
     
+    if len(args) == 0:
+        parser.print_help()
+        return 
+
+    d = options.directory
     cmd = args[0]
+    value = None
+    try:
+        value = args[1]
+    except IndexError:
+        pass
 
     if cmd == 'type':
-        dirtype()
+        dirtype(d, value)
     elif cmd == 'who':
-        who()
+        who(d, value)
     elif cmd == 'what':
-        what()
+        what(d, value)
     elif cmd == 'when':
-        when()
+        when(d, value)
     elif cmd == 'where':
-        where()
+        where(d, value)
+    elif cmd == 'get':
+        get(d)
     else: 
         print "unknown command: %s" % cmd
 
-def dirtype():
-    pass
+def dirtype(d, value):
+    namaste =  _set_namaste(d, 0, value)
+    print "created namaste %s" % namaste
+    return namaste
 
-def who():
-    pass
+def who(d, value):
+    namaste = _set_namaste(d, 1, value)
+    print "created namaste %s" % namaste
+    return namaste
 
-def what():
-    pass
+def what(d, value):
+    namaste = _set_namaste(d, 2, value)
+    print "created namaste %s" % namaste
+    return namaste
 
-def when():
-    pass
+def when(d, value):
+    namaste = _set_namaste(d, 3, value)
+    print "created namaste %s" % namaste
+    return namaste
 
-def where():
-    pass
+def where(d, value):
+    namaste = _set_namaste(d, 4, value)
+    print "created namaste %s" % namaste
+    return namaste
 
+def get(d):
+    tags = []
+    for namaste in glob.iglob(os.path.join(d, "[0-4]=*")):
+        tags.append(namaste)
+    if tags:
+        print "namastes: %s" % ", ".join(tags)
+    return tags
+
+def _set_namaste(d, tag, value):
+    if not value:
+        return None
+    if not os.path.isdir(d):
+        raise Exception("directory %s does not exist" % d)
+    namaste = os.path.join(d, _make_namaste(tag, value))
+    with open(namaste, 'w') as f:
+        f.write(value)
+        f.write("\n")
+    return namaste
+
+def _make_namaste(tag, value):
+    return "%s=%s" % (tag, value)
--- test.py
+++ test.py
@@ -3,30 +3,68 @@ import shutil
 import unittest
 import namaste
 
+TESTDIR = 'namaste-test'
+
+
 class NamasteTests(unittest.TestCase):
 
     def setUp(self):
-        if os.path.isdir('namaste-test'):
-            shutil.rmtree('namaste-test')
-        shutil.copytree('docs', 'namaste-test')
+        if os.path.isdir(TESTDIR):
+            shutil.rmtree(TESTDIR)
+        shutil.copytree('docs', TESTDIR)
 
     def tearDown(self):
-        if os.path.isdir('namaste-test'):
-            shutil.rmtree('namaste-test')
+        if os.path.isdir(TESTDIR):
+            shutil.rmtree(TESTDIR)
 
     def test_type(self):
-        self.assertTrue(True)
+        self.assertFalse(os.path.isfile(os.path.join(TESTDIR, '0=bagit_0.1')))
+        namaste.dirtype(TESTDIR, 'bagit_0.1') 
+        self.assertTrue(os.path.isfile(os.path.join(TESTDIR, '0=bagit_0.1')))
+        self.assertEqual(open(os.path.join(TESTDIR, '0=bagit_0.1')).read(),
+                         "bagit_0.1\n")
 
     def test_who(self):
-        self.assertTrue(True)
+        self.assertFalse(os.path.isfile(os.path.join(TESTDIR, '1=Twain,M.')))
+        namaste.who(TESTDIR, "Twain,M.")
+        self.assertTrue(os.path.isfile(os.path.join(TESTDIR, '1=Twain,M.')))
+        self.assertEqual(open(os.path.join(TESTDIR, '1=Twain,M.')).read(),
+                         "Twain,M.\n")
 
     def test_what(self):
-        self.assertTrue(True)
+        self.assertFalse(os.path.isfile(os.path.join(TESTDIR, '2=Hamlet')))
+        namaste.what(TESTDIR, "Hamlet")
+        self.assertTrue(os.path.isfile(os.path.join(TESTDIR, '2=Hamlet')))
+        self.assertEqual(open(os.path.join(TESTDIR, '2=Hamlet')).read(),
+                         "Hamlet\n")
 
     def test_when(self):
-        self.assertTrue(True)
+        self.assertFalse(os.path.isfile(os.path.join(TESTDIR, '3=2005')))
+        namaste.when(TESTDIR, "2005")
+        self.assertTrue(os.path.isfile(os.path.join(TESTDIR, '3=2005')))
+        self.assertEqual(open(os.path.join(TESTDIR, '3=2005')).read(),
+                         "2005\n")
 
     def test_where(self):
-        self.assertTrue(True)
+        self.assertFalse(os.path.isfile(os.path.join(TESTDIR, '4=Seattle')))
+        namaste.where(TESTDIR, "Seattle")
+        self.assertTrue(os.path.isfile(os.path.join(TESTDIR, '4=Seattle')))
+        self.assertEqual(open(os.path.join(TESTDIR, '4=Seattle')).read(),
+                         "Seattle\n")
 
+    def test_get(self):
+        namaste.dirtype(TESTDIR, 'bagit_0.1') 
+        namaste.who(TESTDIR, "Twain,M.")
+        namaste.what(TESTDIR, "Hamlet")
+        namaste.when(TESTDIR, "2005")
+        namaste.where(TESTDIR, "Seattle")
+        tags = namaste.get(TESTDIR)
+        tags.sort()
+        self.assertEqual(tags, [
+                os.path.join(TESTDIR, namaste._make_namaste(0, 'bagit_0.1')),
+                os.path.join(TESTDIR, namaste._make_namaste(1, 'Twain,M.')),
+                os.path.join(TESTDIR, namaste._make_namaste(2, 'Hamlet')),
+                os.path.join(TESTDIR, namaste._make_namaste(3, '2005')),
+                os.path.join(TESTDIR, namaste._make_namaste(4, 'Seattle')),
+                ])
 
